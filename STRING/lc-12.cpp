51d1bb983b8e43c323d0aca9de28f4eb2acf505d
Bulbul verma <bulbulverma6266@gmail.com>
2026-07-02 12:03:33 +0530

    Added c++ dsa solutions

---
+++ STRING/lc-12.cpp
@@ -0,0 +1,23 @@
+ // medium problem     roman   
+
+
+class Solution {
+public:
+    string intToRoman(int num) {
+      static vector<int>value{1000, 900, 500, 400, 100, 90, 50, 40 ,10,9,5,4,1} ;
+     static vector<string>symbol{"M","CM","D","CD", "C","XC","L","XL","X","IX","V","IV","I"};
+
+     string result ="";
+     for(int i = 0 ;i<13; i++){
+        if(num==0) break;
+
+        int times = num/value[i];
+        while(times != 0){
+            result+= symbol[i];
+            times--;
+        }
+        num = num % value[i];
+     }
+      return result ;
+    }
+};
